e8cc56ed2b21428493ff6086049c07b50b5476a0
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-04-08 16:03:29 +0200

    end

---
+++ script.js
@@ -0,0 +1,5 @@
+let name=prompt('GET YOUR NAME');
+let lastname=prompt('GET YOUR SURNAME');
+let color=prompt('GET YOUR COLOR');
+const newp= name + lastname + color;
+document.getElementById("password").innerHTML = newp
